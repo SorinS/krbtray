60e5ff968decda3cefeca05d2e2bdece4a05ab35
sorins <ssuciu@gmail.com>
2025-12-06 21:04:52 +0000

    native clipboard

--- clipboard_darwin.go
+++ clipboard_darwin.go
@@ -3,13 +3,25 @@
 
 package main
 
-import (
-	"os/exec"
-	"strings"
-)
+/*
+#cgo CFLAGS: -x objective-c
+#cgo LDFLAGS: -framework Cocoa
+
+#import <Cocoa/Cocoa.h>
+
+void copyToClipboardNative(const char *text) {
+    NSString *str = [NSString stringWithUTF8String:text];
+    NSPasteboard *pasteboard = [NSPasteboard generalPasteboard];
+    [pasteboard clearContents];
+    [pasteboard setString:str forType:NSPasteboardTypeString];
+}
+*/
+import "C"
+import "unsafe"
 
 func copyToClipboardPlatform(text string) error {
-	cmd := exec.Command("pbcopy")
-	cmd.Stdin = strings.NewReader(text)
-	return cmd.Run()
+	cstr := C.CString(text)
+	defer C.free(unsafe.Pointer(cstr))
+	C.copyToClipboardNative(cstr)
+	return nil
 }
